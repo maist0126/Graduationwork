a7e63e4ffb0a31ab83ab5d4f94621b9e515b963c
MDNG <38938722+maist0126@users.noreply.github.com>
2019-05-01 01:51:33 +0900

    updated

--- timer.js
+++ timer.js
@@ -1,13 +1,5 @@
-// $(document).ready(function(){
-//   if (start_status == 1){
-//     console.log("s");
-//     tid=setInterval('msg_time()',1000);
-//   } // 타이머 1초간격으로 수행
-// });
-
 var start_status = 1;
 var RemainDate = 1000*60*60*1;
-
 
 function msg_time() {
   var hours = Math.floor((RemainDate % (1000 * 60 * 60 * 24)) / (1000*60*60));
@@ -17,9 +9,6 @@ function msg_time() {
   m = hours + ":" +  minutes + ":" + seconds ; // 남은 시간 text형태로 변경
   
   document.all.timer.innerHTML = m;   // div 영역에 보여줌 
-  
-  var hd = document.getElementById("header");
-  hd.style.backgroundColor = '#aaaaaa' ;
 
   if (RemainDate < 0) {      
     // 시간이 종료 되었으면..
@@ -29,10 +18,14 @@ function msg_time() {
   }
 }
 
+function original(){
+  var hd = document.getElementById("header");
+  hd.style.backgroundColor = '#aaaaaa' ;  
+}
+
 function start(){
   if (start_status == 1){
     start_status = 0;
-    console.log("s");
     tid=setInterval('msg_time()',1000);
   }
 }
@@ -41,10 +34,12 @@ function add(){
 	RemainDate = RemainDate + 1000*10;
   var hd = document.getElementById("header");
   hd.style.backgroundColor = '#0000ff' ;
+  setTimeout("original()", 500);
 }
 
 function subtract(){
 	RemainDate = RemainDate - 1000*10;
   var hd = document.getElementById("header");
   hd.style.backgroundColor = 'red' ;
+  setTimeout("original()", 500);
 }
