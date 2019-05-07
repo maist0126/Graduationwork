4c0e1cfbed407d69a4c90f109a43f409c3b81e16
MDNG <38938722+maist0126@users.noreply.github.com>
2019-05-08 02:00:12 +0900

    timer updated

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var order = 1;
-var real_order = 0;
+var order;
+var real_order;
 
 var current_username;
 var next_username;
 
-var RemainDate = 1000*20;
+var RemainDate;
 
 var start_status = 1;
 
@@ -23,17 +23,26 @@ var start_status = 1;
 	
 	firebase.database().ref().child('order').on('value', initialData);
 	update_order();
-
+	firebase.database().ref('order/'+real_order).set({
+		username: current_username,
+		time: RemainDate,
+		order: real_order
+	});
+	firebase.database().ref().child('order').on('value', gotTime);
+	console.log(RemainDate);
+	console.log(order);
+	console.log(real_order);
 }());
 
 function speak(userId){
 	firebase.database().ref('order/'+order 
 		).set({
 		username: userId,
-		time: 120,
+		time: 20000,
 		order: order
 	});
 	order += 1;
+
 	firebase.database().ref('order/'+0).set({
 		order: order,
 		real_order: real_order,
@@ -44,20 +53,27 @@ function speak(userId){
 }
 
 function msg_time() {
-  var hours = Math.floor((RemainDate % (1000 * 60 * 60 * 24)) / (1000*60*60));
-  var minutes = Math.floor((RemainDate % (1000 * 60 * 60)) / (1000*60));
-  var seconds = Math.floor((RemainDate % (1000 * 60)) / 1000);
+	firebase.database().ref('order/'+real_order).set({
+		username: current_username,
+		time: RemainDate,
+		order: real_order
+	});
+	firebase.database().ref().child('order').on('value', gotTime);
+	  var hours = Math.floor((RemainDate % (1000 * 60 * 60 * 24)) / (1000*60*60));
+	  var minutes = Math.floor((RemainDate % (1000 * 60 * 60)) / (1000*60));
+	  var seconds = Math.floor((RemainDate % (1000 * 60)) / 1000);
 
-  m = hours + ":" +  minutes + ":" + seconds ; // 남은 시간 text형태로 변경
-  
-  document.all.timer.innerHTML = m;   // div 영역에 보여줌 
+	  m = hours + ":" +  minutes + ":" + seconds ; // 남은 시간 text형태로 변경
+	  
+	  document.all.timer.innerHTML = m;   // div 영역에 보여줌 
 
-  if (RemainDate <= 0) {      
-    // 시간이 종료 되었으면..
-    stop();  // 타이머 해제
-  }else{
-    RemainDate = RemainDate - 1000; // 남은시간 -1초
-  }
+	  if (RemainDate <= 0) {      
+	    // 시간이 종료 되었으면..
+	    stop();  // 타이머 해제
+	  }else{
+	    RemainDate = RemainDate - 1000;
+	 // 남은시간 -1초
+	  }
 }
 
 function original(){
@@ -76,6 +92,7 @@ function start(){
 		});
   		update_order();
   		start_status = 0;
+  		firebase.database().ref().child('order').on('value', gotTime);
     	tid=setInterval('msg_time()',1000);
     	console.log(real_order);
   	}
@@ -86,7 +103,7 @@ function stop(){
 	clearInterval(tid);
 	firebase.database().ref('order/'+real_order).set({
 		username: current_username,
-		time: RemainDate/1000,
+		time: RemainDate,
 		order: real_order
 	});
 	RemainDate = 1000*20;	
@@ -97,6 +114,9 @@ function initialData(data){
 	real_order = data.val()[0].real_order;
 }
 
+function gotTime(data){
+	RemainDate = data.val()[real_order].time;
+}
 function gotData(data){
 	try {
 		current_username = data.val()[real_order].username;
